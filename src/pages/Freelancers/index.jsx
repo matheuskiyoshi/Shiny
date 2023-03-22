7fa9fc1e932c8c34aa362161a4b5818138fc0912
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-03-22 18:44:50 -0300

    usando hooks, useState e useEffect, alterando css do survey, e utilizando fetch para pegar dados do api

--- src/pages/Freelancers/index.jsx
+++ src/pages/Freelancers/index.jsx
@@ -1,11 +1,26 @@
 import Card from "../../components/Card"
 import styled from "styled-components"
+import colors from "../../utils/style/colors"
 
 const CardsContainer = styled.div`
    display: grid;
    gap: 24px;
    grid-template-rows: 350px 350px;
    grid-template-columns: repeat(2, 1fr);
+   align-items: center;
+`
+const PageTitle = styled.h1`
+   font-size: 30px;
+   color: black;
+   text-align: center;
+   padding-bottom: 30px;
+`
+const PageSubtitle = styled.h2`
+   font-size: 20px;
+   color: ${colors.secondary};
+   font-weigth: 300;
+   text-align: center;
+   padding-bottom: 30px;
 `
 
 const freelancerProfiles = [
@@ -27,7 +42,10 @@ function Freelancers() {
     
     return (
        <div>
-          <h1>Freelancers 👩·💻👨·💻👩·💻</h1>
+          <PageTitle>Find your service provider</PageTitle>
+          <PageSubtitle>
+            Here at Shiny we bring together the best profiles for you.
+          </PageSubtitle>
           <CardsContainer>
           {freelancerProfiles.map((profile, index) => (
               <Card
--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,23 +1,81 @@
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import { useState,useEffect } from 'react'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+import { Loader } from '../../utils/style/Atoms'
+
+const SurveyContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+const QuestionTitle = styled.h2`
+    text-decoration: underline;
+    text-decoration-color: ${colors.primary}
+`
+const QuestionContent = styled.span`
+    margin: 30px;
+`
+const LinkWrapper = styled.div`
+    padding-top: 30px;
+    & a {
+        color: black;
+    }
+    & a:first-of-type {
+        margin-right: 20px;
+    }
+`
 
 function Survey() {
     const { questionNumber } = useParams()
     const questionNumberInt = parseInt(questionNumber)
     const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
     const nextQuestionNumber = questionNumberInt + 1
+    const [surveyData, setSurveyData] = useState({})
+    const [isDataLoading, setDataLoading] = useState(false)
+    const [error, setError] = useState(false)
 
-    return (
-        <div>
-            <h1>Survey</h1>
-            <h2>Question {questionNumber}</h2>
-            <Link to={`/survey/${prevQuestionNumber}`}>Back</Link>
-            {questionNumberInt === 10 ? (<Link to='/results'>Results</Link>
-            ) : (
-                <Link to={`/survey/${nextQuestionNumber}`}>Next</Link>
-            ) }
-        </div>
-    )
+
+    useEffect(() => {
+        async function fetchSurvey() {
+          setDataLoading(true)
+          try {
+            const response = await fetch(`http://localhost:8000/survey`)
+            const { surveyData } = await response.json()
+            setSurveyData(surveyData)
+          } catch (err) {
+            console.log('===== error =====', err)
+            setError(true)
+          } finally {
+            setDataLoading(false)
+          }
+        }
+        fetchSurvey()
+      }, [])
+
+      if (error) {
+        return <span>Oops! There is an error</span>
+      }
+
+return (
+    <SurveyContainer>
+      <QuestionTitle>Question {questionNumber}</QuestionTitle>
+      {isDataLoading ? (
+        <Loader />
+      ) : (
+        <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
+      )}
+      <LinkWrapper>
+        <Link to={`/survey/${prevQuestionNumber}`}>Back</Link>
+        {surveyData[questionNumberInt + 1] ? (
+          <Link to={`/survey/${nextQuestionNumber}`}>Next</Link>
+        ) : (
+          <Link to="/results">Results</Link>
+        )}
+      </LinkWrapper>
+    </SurveyContainer>
+  )
 }
 
 export default Survey
