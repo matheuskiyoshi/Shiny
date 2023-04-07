7a0c58bb6999ed4ac630fd20a2f90346f6668140
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-04-07 14:55:21 -0300

    Adicionando testes a página resultados

--- src/pages/Results/index.test.js
+++ src/pages/Results/index.test.js
@@ -1,4 +1,9 @@
-import { formatJobList, formatQueryParams } from ".";
+import Results, { formatJobList, formatQueryParams } from ".";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { waitForElementToBeRemoved, screen } from "@testing-library/react";
+import { render } from "../../utils/test";
+
 
 describe('The formatJobList function', () => {
     it('should add a comma to a word', () => {
@@ -19,5 +24,33 @@ describe('The formatQueryParams function', () => {
     it('should concatenate params with an &', () => {
         const expectedState = 'a1=answer1&a2=answer2'
         expect(formatQueryParams({ 1: 'answer1', 2: 'answer2' })).toEqual(expectedState)
+    })
+})
+
+const resultsMockedData = [
+    {
+        title: 'seo',
+        description: `The seo is in charge of the web referencing of a page`,
+    },
+]
+
+const server = setupServer(
+    rest.get('http://localhost:8000/results', (req, res, ctx) => {
+        return res(ctx.json({resutsData: resultsMockedData}))
+    })
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+describe('The Results component', () => {
+    test('Should display the results after the data is loaded', async () => {
+        render(<Results />)
+        await waitForElementToBeRemoved(() => screen.queryByTestId('loader'))
+        const jobTitleElements = screen.queryAllByTestId('job-title')
+        expect(jobTitleElements.textContent).toBe(resultsMockedData.title)
+        const jobDescriptionElements = screen.queryAllByTestId('job-description')
+        expect(jobDescriptionElements.textContent).toBe(resultsMockedData.description)
     })
 })
